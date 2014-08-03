908e4afbd6d4cddc0c8ea49fb2c4fb15d843b91a
Daniel Bonilla <dbonilla21@gmail.com>
2014-08-03 17:57:21 -0600

    fixed categories add

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -21,14 +21,14 @@ class CategoriesController extends UndoController {
  *
  * @return void
  */
- 
+
 	public function index() {
 		$this->Category->recursive = 0;
 		$this->set('categories', $this->Paginator->paginate());
 	}
 
 /**
- * view method 
+ * view method
  *
  * @throws NotFoundException
  * @param string $id
@@ -52,7 +52,7 @@ class CategoriesController extends UndoController {
 			$this->Category->create();
 			if ($this->Category->save($this->request->data)) {
 				$this->Session->setFlash(__('The category has been saved.'));
-        $this->createMemento('add', array('type' => 'category', 'id' => $this->Category->id));
+        $this->createMemento('add', 'category', array('id' => $this->Category->id));
 				return $this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The category could not be saved. Please, try again.'));
