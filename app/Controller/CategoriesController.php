3020ee89b1460487bdc2ba78e0539509e9c092c8
gomezjosetomas <gomezjosetomas@gmail.com>
2014-08-03 17:45:19 -0600

    Units list removed.

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -21,19 +21,20 @@ class CategoriesController extends UndoController {
  *
  * @return void
  */
+ 
 	public function index() {
 		$this->Category->recursive = 0;
 		$this->set('categories', $this->Paginator->paginate());
 	}
 
 /**
- * view method
+ * view method 
  *
  * @throws NotFoundException
  * @param string $id
  * @return void
  */
-	public function view($id = null) {
+	public function advanceView($id = null) {
 		if (!$this->Category->exists($id)) {
 			throw new NotFoundException(__('Invalid category'));
 		}
