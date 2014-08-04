e36f2de410cc41a6e51e47a7ce26cb0923728165
gomezjosetomas <gomezjosetomas@gmail.com>
2014-08-04 15:25:53 -0600

    test database configured

--- app/Config/database.php
+++ app/Config/database.php
@@ -9,4 +9,13 @@ class DATABASE_CONFIG {
 		'password' => '',
 		'database' => 'inventario',
 	);
+	
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host'       => 'localhost',
+		'login'      => 'root',
+		'password'   => '',
+		'database'   => 'test_database'
+);
 }
